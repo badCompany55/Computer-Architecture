e7df126426cc6c615377473bb5a997652a1637ec
badCompany55 <zachirvin45@gmail.com>
2019-11-02 14:14:56 -0500

    day two

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,7 +4,8 @@ import sys
 
 HTL = 0b00000001 # HLT Stops the run
 LDI = 0b10000010 # LDI R0,8
-PRN = 0b01000111, # PRN R0
+PRN = 0b01000111 # PRN R0
+MUL = 0b10100010 # MUL R0,R1
 
 class CPU:
     """Main CPU class."""
@@ -21,22 +22,38 @@ class CPU:
     def ram_write(self, MDR, MAR):
         self.ram[MAR] = MDR
 
-    def load(self):
+    def load(self, file):
         """Load a program into memory."""
-
         address = 0
+        
+        program = []
+        f = open(f'./examples/{file}', "r")
+        fr = f.readlines()
+        for x in fr:
+            if "#" not in x and x.strip():
+                new_x = int(x, 2)
+                program.append(new_x)
+            elif " #" in x:
+                index = x.index(" ")
+                split = x[0:index + 1]
+                new_x = int(split, 2)
+                program.append(new_x)
+            
+                
+
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            LDI, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            PRN, # PRN R0
-            0b00000000,
-            HTL,
-        ]
+       # program = [
+       #     # From print8.ls8
+       #     LDI, # LDI R0,8
+       #     0b00000000,
+       #     0b00001000,
+       #     PRN, # PRN R0
+       #     0b00000000,
+       #     HTL,
+       # ]
+    
 
         for instruction in program:
             self.ram[address] = instruction
@@ -46,9 +63,12 @@ class CPU:
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+        if op == "ALU_ADD":
+            new_val = self.reg[reg_a] + self.reg[reg_b]
+        elif op == "ALU_MUL":
+            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
         #elif op == "SUB": etc
+            
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -84,8 +104,14 @@ class CPU:
             # instructions state that up to 2 memory slots will be used
             # using ram_read, get the next 2 commands and store them
             # in operand_a / operand_b
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
+            
+            number_of_opers = IR >> 6
+            if number_of_opers == 2:
+                operand_a = self.ram_read(self.pc + 1)
+                operand_b = self.ram_read(self.pc + 2)
+            else:
+                operand_a = self.ram_read(self.pc + 1)
+
 
             if IR == HTL:
                 self.pc = 0
@@ -94,8 +120,11 @@ class CPU:
                 self.reg[operand_a] = operand_b
             elif IR == PRN:
                 print(self.reg[operand_a])
+            elif IR == MUL:
+                self.alu("ALU_MUL", operand_a, operand_b)
+               
             #    return self.reg[operand_a]
-            self.pc += 1
+            self.pc += number_of_opers + 1
             
         
         
